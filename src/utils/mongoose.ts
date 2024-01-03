f3352245d9cc1324ff5acf1ca81e7edb1dae3280
peravihaymoshe <peravihaymoshe@gmail.com>
2024-01-03 16:56:52 +0200

    add logs

--- src/utils/mongoose.ts
+++ src/utils/mongoose.ts
@@ -1,17 +1,18 @@
 import mongoose from 'mongoose'
 
-const connectWithRetry = async (): Promise<void> => {
+const connectWithRetry = async (R_NUM = 10): Promise<void> => {
   const dbHost = process.env.DB_HOST ?? 'mongodb+srv://store_23:store_23@cluster0.kcamuno.mongodb.net/stores_db?retryWrites=true&w=majority'
+  console.log('connection nstring: ' + dbHost)
   let isConnect = false
-  for (let retries = 10; retries > 0; retries--) {
+  let retries = R_NUM
+  while (!isConnect && retries > 0) {
     try {
       await mongoose.connect(dbHost)
       isConnect = true
       console.log('Connected to MongoDB')
-      break
     } catch (error) {
       console.error('Error connecting to MongoDB:', error)
-      console.log(`Retries left: ${retries}`)
+      console.log(`Retries left: ${--retries}`)
       await new Promise((resolve) => setTimeout(resolve, 5000)) // Wait for 5 seconds before retrying
     }
   }
@@ -22,10 +23,9 @@ const connectWithRetry = async (): Promise<void> => {
 
 export const connectToDatabase = async (): Promise<void> => {
   try {
-    await connectWithRetry()
+    await connectWithRetry(10)
   } catch (error) {
     console.error('Error connecting to MongoDB:', error)
-    process.exit(1)
   }
 }
 
